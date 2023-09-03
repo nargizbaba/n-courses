b1f480277eff4187a46237bc9d8c01f1f4454545
nargizbaba <nergizb99@gmail.com>
2023-09-03 23:12:31 +0400

    FirstCommit

---
+++ DataTypes/Program.cs
@@ -0,0 +1,39 @@
+﻿//server side
+string s_username = "admins";
+string s_password = "who";
+bool isLogged = false;
+int tryCount = 0;
+int chancesLeft = 0;
+const int MAX_TRY_COUNT = 17;
+
+//client side
+if (isLogged == false)
+{
+    LOG:
+    Console.WriteLine("enter username");
+    string c_username = Console.ReadLine();
+    Console.WriteLine("enter password");
+    string c_password = Console.ReadLine();
+
+    if (c_username == s_username && c_password == s_password)
+    {
+        isLogged = true;
+        Console.WriteLine("You logged in successfully!");
+    }
+    else
+    {
+        Console.WriteLine("Try again");
+        tryCount++;
+        chancesLeft = MAX_TRY_COUNT - tryCount;
+        Console.WriteLine("you have chances: " + chancesLeft);
+        if (tryCount == MAX_TRY_COUNT)
+        {
+            Console.WriteLine("you banned");
+        }
+        goto LOG;
+    }
+}
+
+
+
+
